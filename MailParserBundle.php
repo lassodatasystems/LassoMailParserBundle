ea03dda1cc172d214b262e5f0d0e4683a9f3ed68
Thomas Rubbert <thomas@lassocrm.com>
2013-01-28 17:00:27 -0800

    Fixed name

--- MailParserBundle.php
+++ MailParserBundle.php
@@ -2,7 +2,6 @@
 namespace Lasso\MailParserBundle;
 
 use Zend\Mail\Storage\Part;
-use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
  * Provides a very simple wrapper around the zend mail library. Contains assorted helper functions regarding mail
@@ -10,10 +9,6 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class LassoMailParserBundle extends Bundle
 {
-    /**
-     * @var Part
-     */
-    protected $mail = null;
 
     /**
      * Returns the name of the class.
@@ -23,54 +18,5 @@ class LassoMailParserBundle extends Bundle
     public function getName()
     {
         return 'LassoMailParserBundle';
-    }
-
-    /**
-     * Accepts the raw email as a string, including headers and body. Has to be called before the other functions
-     * are available.
-     *
-     * @param string $mail
-     */
-    public function parse($mail)
-    {
-        $this->mail = new Part(['raw' => $mail]);
-    }
-
-    /**
-     * Returns the mail object. $this->parse() has to be called before this function is available.
-     *
-     * @return null|\Zend\Mail\Storage\Part
-     * @throws \LogicException
-     */
-    public function getMail()
-    {
-        if (empty($this->mail)) {
-            throw new \LogicException('You must first call $this->parse()');
-        }
-
-        return $this->mail;
-    }
-
-    /**
-     * Returns all email addresses contained in the email headers. This includes, to, from, cc, and bcc.
-     * $this->parse() has to be called before this function is available.
-     *
-     * @return array
-     * @throws \LogicException
-     */
-    public function getAllEmailAddresses()
-    {
-        if (empty($this->mail)) {
-            throw new \LogicException('You must first call $this->parse()');
-        }
-
-        $addresses = [];
-        foreach ($mail->getHeader('to')->getAddressList() as $address) {
-            $addresses[] = $address->getEmail();
-        }
-
-        $addresses = array_unique($addresses);
-
-        return $addresses;
     }
 }
---
+++ Parser.php
@@ -0,0 +1,60 @@
+<?php
+namespace Lasso\MailParserBundle;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class Parser
+{
+    /**
+     * @var Part
+     */
+    protected $mail = null;
+
+    /**
+     * Accepts the raw email as a string, including headers and body. Has to be called before the other functions
+     * are available.
+     *
+     * @param string $mail
+     */
+    public function parse($mail)
+    {
+        $this->mail = new Part(['raw' => $mail]);
+    }
+
+    /**
+     * Returns the mail object. $this->parse() has to be called before this function is available.
+     *
+     * @return null|\Zend\Mail\Storage\Part
+     * @throws \LogicException
+     */
+    public function getMail()
+    {
+        if (empty($this->mail)) {
+            throw new \LogicException('You must first call $this->parse()');
+        }
+
+        return $this->mail;
+    }
+
+    /**
+     * Returns all email addresses contained in the email headers. This includes, to, from, cc, and bcc.
+     * $this->parse() has to be called before this function is available.
+     *
+     * @return array
+     * @throws \LogicException
+     */
+    public function getAllEmailAddresses()
+    {
+        if (empty($this->mail)) {
+            throw new \LogicException('You must first call $this->parse()');
+        }
+
+        $addresses = [];
+        foreach ($this->mail->getHeader('to')->getAddressList() as $address) {
+            $addresses[] = $address->getEmail();
+        }
+
+        $addresses = array_unique($addresses);
+
+        return $addresses;
+    }
+}
